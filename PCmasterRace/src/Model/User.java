b6647a26eda35d5f65decab80c630d77c63b8b31
Superbi <joao.superbi1608@gmail.com>
2025-10-22 22:57:09 -0300

    Controller Login

--- PCmasterRace/src/Model/User.java
+++ PCmasterRace/src/Model/User.java
@@ -23,26 +23,35 @@ public class User {
     protected String nome;
     protected int idade;
     private String cpf;
+    private String login;
+    private String senha;
+
+    public User(String login, String senha) {
+        
+    }
     
     public User() {
         this.id = 0;
         this.nome = "";
         this.idade = 0;
-        this.cpf = "000.000.000-00";   
+        this.cpf = "000.000.000-00"; 
+        this.login = "";
+        this.senha = "";
     }
     
-    public User(int id, String nome, int idade, String cpf) {
+    public User(int id, String nome, int idade, String cpf, String login, String senha) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
         this.cpf = cpf;
+        this.login = login;
+        this.senha = senha;
     } 
 
-    public String toString() {
-        String txt = "ID: "+ this.id + "Nome: " + this.nome +"\n"
-        + "\n" +"Idade: "+ this.idade+"\n" + "CPF: " + this.cpf + "\n";
-        return txt;
-    }   
+    //Criar usuario Teste
+    public static User criarUsuarioTeste(String login, String senha) {
+        return new User(1, "Usuário Teste", 25, "111.111.111-11", login, senha);
+    }
     
     @Override
     public boolean equals(Object obj) {
@@ -80,6 +89,45 @@ public class User {
         this.idade = idade;
     }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", nome=" + nome + ", idade=" + idade + ", cpf=" + cpf + ", login=" + login + ", senha=" + senha + '}';
+    }
+    
+
+    
 }
 
 
--- PCmasterRace/src/controller/LoginController.java
+++ PCmasterRace/src/controller/LoginController.java
@@ -4,15 +4,25 @@
  */
 package controller;
 
+import Model.User;
+
 /**
  *
  * @author gabri
  */
 public class LoginController {
-    public boolean loginValidate(String login, String senha){
-      //  if()
-        return false;
-      //  if()
+    
+        
+    public boolean validarLogin(User usuario, String login, String senha) {
+        
+        User usuarioCustom = User.criarUsuarioTeste("admin", "12345");
+      
+        if (usuario == null || login == null || senha == null ) {
+            return false;
+        }
+            return usuario.getLogin().equals(login) && usuario.getSenha().equals(senha);
         
     }
+    
+ 
 }
